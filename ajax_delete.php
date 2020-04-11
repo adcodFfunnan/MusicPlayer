97fb7c73cdd0cc8cd744cb8047f24ae2f6f3e9d9
Adnan Birparic <adnan.birparic@gmail.com>
2020-04-11 15:50:12 +0200

    Delete ajax_delete.php

--- ajax_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id_for_delete=$_GET['name1'];
-$database=$_GET['name2'];
-include 'php_methods.php';
-$con=make_connect('3361977_users');
-mysqli_query($con,"DELETE FROM $database WHERE id=$id_for_delete");
-mysqli_close($con);
